983a49530eb28b4077108fd24e90bb91034a2a0b
fiopwk <dajuan-mcdonald@lambdastudents.com>
2022-03-30 17:24:23 -0400

    code cleanup

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 echo "This program takes input of integer and outputs either string or integer: "
-
 echo " "
 echo "The range of 1 to your input determins if the output will be either: "
 echo " "
 echo " Fizz, Buzz, or FizzBuzz"
 echo " "
 echo " If Fizz, the number is divisible by 3"
+echo " "
 echo " If Buzz, the number is divisible by 5"
+echo " "
 echo " if FizzBuzz, the number is divisible by 3 and 5"
+echo " "
 echo " otherwise the output will just be a number...or nothing!"
-read -p "Enter a number between 1 and 100: " target
 
 
-function fizzbuzz {
+function fizzbuzz() {
 
 Fizz=3
 Buzz=5
 FizzBuzz=15
+read -p "Enter a number between 1 and 100: " target
 
 for ((i=1; i < $target; i++));
     do 
@@ -42,3 +44,4 @@ done;
 }
 
 fizzbuzz
+
