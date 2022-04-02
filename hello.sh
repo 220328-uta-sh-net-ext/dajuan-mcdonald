b668a08e13875584c7608da58c22ff4b28183698
fiopwk <flopwiki@gmail.com>
2022-04-02 12:55:13 -0400

    added system variable to output interactivity

--- hello.sh
+++ hello.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # prompt the user
-read -p "Hello, what is your name? " username
+read -p "Hello, what is your name? " given_username
 
 # greet the user by name
-echo "Hello, $username! Thank you for using this program"
+echo "Hello, $given_username! Thank you for using this program"
+echo "We have detected that your system username is :  $USERNAME"
 
