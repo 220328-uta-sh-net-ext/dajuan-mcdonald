ed3fd040e5869fb135e9baf4c7f3c4156fc4dbeb
fiopwk <flopwiki@gmail.com>
2022-04-04 22:08:21 -0400

    added more code

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Hello, Ubuntu!");
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine(15 % 30);
+
+string userName = "fiopwk";
+
+
+// Basic string formatting in C#
+string repo = $@"220328-uta-sh-net-ext/{userName}";
+Console.WriteLine(repo);
+
+
+
