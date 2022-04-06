3299c0831bf0a2933a880eb6777fadb86936a0d5
fiopwk <flopwiki@gmail.com>
2022-04-05 22:29:58 -0400

    Microsoft Learn content

---
+++ B/training-material/MicrosoftLearn/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ *Knowledge Check:
+ *
+ *
+ *
+ * 1. What will be the result of the following line of code?
+ *  - Console.WriteLine(5 / 10);
+ */
+Console.WriteLine(5 / 10); // 0
+
+/*
+ * 2. Which of the following lines of code uses the addition assignment
+ * operator?
+ *
+ * value +=5;
+ * value++;
+ * value = value + 5;
+ * value && 5;
+ *
+ * */
+
+int value = 1;
+value += 5; 
+
+Console.WriteLine(value); // 6
