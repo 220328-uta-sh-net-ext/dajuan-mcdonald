32317457bc597d235816e8660beb25d1b3ce2a5a
fiopwk <flopwiki@gmail.com>
2022-04-15 20:17:05 -0400

    switched to linux

--- C/RestaurantDL/Program.cs
+++ C/RestaurantDL/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(){
+        Console.WriteLine("Data Level Logic");
+    }
+
+}
