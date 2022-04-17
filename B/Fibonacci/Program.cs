b1bac2ca6a0e62ee8cefebc3c0581320249f42b0
fiopwk <flopwiki@gmail.com>
2022-04-17 18:04:16 -0400

    fixed conditional bug that should now return correct value

--- B/Fibonacci/Program.cs
+++ B/Fibonacci/Program.cs
@@ -7,11 +7,12 @@ static int Fibonacci(int n)
     int n2 = 1;
     int sum;
 
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i <= n; i++)
     {
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
     }
     return n == 0 ? n1 : n2;
+    
 }
