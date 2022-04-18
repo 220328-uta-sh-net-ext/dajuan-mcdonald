8d1084287c333cf43199ad4618a8c927107874f4
fiopwk <flopwiki@gmail.com>
2022-04-17 22:24:05 -0400

    restaruant rating plugged in and showing rating

--- C-Advanced/RestaurantApp/Program.cs
+++ C-Advanced/RestaurantApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RestaurantApp
+{
+    public class Restaurant
+    {
+        private readonly string m_restaurantName;
+        private int m_rating;
+
+        private Restaurant() {}
+
+        public Restaurant(string restaurantName, int restaurantRating)
+        {
+            m_restaurantName = restaurantName;
+            m_rating = restaurantRating;
+        }
+
+        public string RestaurantName
+        {
+            get => m_restaurantName;
+        }
+
+        public int Rating 
+        {
+            get => m_rating;
+        }
+
+        public void Rate(int rating)
+        {
+            m_rating += rating;
+        }
+
+
+        public static void Main()
+        {
+            Restaurant a = new Restaurant("Billies", 2);
+            Console.WriteLine(a.m_rating);
+        }
+
+    }
+}
