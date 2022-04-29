26c7c0c89fa91203686f5a3d4195f6150f120c17
fiopwk <flopwiki@gmail.com>
2022-04-29 05:39:25 -0400

    dotnet added first Service skipped tests

--- Project0/RestaurantService/IModelService.cs
+++ Project0/RestaurantService/IModelService.cs
@@ -2,6 +2,18 @@ namespace RestaurantService
 {
     interface IServiceInterface
     {
+        //properties
+        string Name {get; set;}
+        decimal Rating {get; set;}
+        string Review {get; set;}
+
+        string UserPassword {set;}
+        string UserName {get; set;}
+
+
+
+
+        //methods
         void SearchBusinessBy();
         void SearchBusinessMethod();
         void AddNewUser();
--- Project0/Tests/UnitTest1.cs
+++ Project0/Tests/UnitTest1.cs
@@ -1,11 +1,36 @@
 using Xunit;
+using RestaurantService;
 
 namespace Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestUserNameIsCalled()
+    {
+
+    }
+
+    [Fact]
+    public void TestAddReviewIsCalledOnce()
+    {
+
+    }
+
+    [Fact]
+    public void TestAddNewUserIsCalledOnce()
+    {
+
+    }
+
+    [Fact]
+    public void TestSearchBusinessMethodIsCalledOnce()
+    {
+
+    }
+
+    [Fact]
+    public void TestSearchBusinessByIsCalledOnce()
     {
 
     }
