08ae59c56312bebcb1ace836048dc1245f0e9ee8
fiopwk <flopwiki@gmail.com>
2022-04-30 19:29:17 -0400

    moving in: simple is about to change

--- Project0/RestaurantServiceUI/Program.cs
+++ Project0/RestaurantServiceUI/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestaurantService;
+
+bool repeat = true;
+while(repeat)
+{
+    string reply = System.Console.ReadLine();
+
+    switch (reply)
+    {
+        case "Search":
+            //do something here
+            System.Console.WriteLine("searching disabled til login satisfied");
+            break;
+        case "Login":
+            //logging in is for admin and user
+            System.Console.WriteLine("...................>>");
+            break;
+        case "Home" :
+            //redirect via console
+             
+        case "Exit" : 
+            repeat = false;
+            break;
+
+        default:
+            System.Console.ReadLine();
+            break;
+
+    }
+}
