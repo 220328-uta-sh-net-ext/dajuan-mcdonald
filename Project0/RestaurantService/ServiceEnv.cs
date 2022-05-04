3bfc198c1139fe7e9b3347aaf73e4e3ac0bf2035
fiopwk <flopwiki@gmail.com>
2022-05-04 15:33:17 -0400

    debrief our actions with a comment

--- Project0/RestaurantService/ServiceEnv.cs
+++ Project0/RestaurantService/ServiceEnv.cs
@@ -4,9 +4,10 @@ namespace RestaurantService
     {
        // var getValue {get; set;}
        // var setValue {get; set;}
-
-       // var getValue = System.Environment.GetEnvironmentVariable(variable [, Target]);
-
-       // var setValue = System.Environment.SetEnvironmentVariable(variable, value [, Target]);
+        
+       //Retrieves the value of an environment variable.
+       var getValue = System.Environment.GetEnvironmentVariable(variable [, Target]);
+       //Creates, modifies, or deletes an environment variable. 
+       var setValue = System.Environment.SetEnvironmentVariable(variable, value [, Target]);
     }
 }
