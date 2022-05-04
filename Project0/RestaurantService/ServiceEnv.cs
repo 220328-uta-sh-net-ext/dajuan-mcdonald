9543a5bccaaac9d081d1ac321e0cb079d00206cf
fiopwk <flopwiki@gmail.com>
2022-05-04 15:26:35 -0400

    Using System.Environment class with enum type parameter

---
+++ Project0/RestaurantService/ServiceEnv.cs
@@ -0,0 +1,12 @@
+namespace RestaurantService
+{
+    public class ServiceEnv
+    {
+       // var getValue {get; set;}
+       // var setValue {get; set;}
+
+       // var getValue = System.Environment.GetEnvironmentVariable(variable [, Target]);
+
+       // var setValue = System.Environment.SetEnvironmentVariable(variable, value [, Target]);
+    }
+}
