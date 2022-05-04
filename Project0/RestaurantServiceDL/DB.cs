f280fae7183f24e811be3aa34db96fd9f93eae34
fiopwk <flopwiki@gmail.com>
2022-05-04 15:15:47 -0400

    change data type parameter from var to string type

--- Project0/RestaurantServiceDL/DB.cs
+++ Project0/RestaurantServiceDL/DB.cs
@@ -1,7 +1,7 @@
 ﻿namespace RestaurantServiceDL;
 public class Class1
 {
-    public static void Connection(var servername)
+    public static void Connection(string servername)
     {
         string server = "juanmcdonald.database.windows.net";
 
