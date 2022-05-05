2198e74294ef0669e95cd30143c29559297241be
fiopwk <flopwiki@gmail.com>
2022-05-05 10:44:41 -0400

    update ratings model

--- Project0/RestaurantService/Review.cs
+++ Project0/RestaurantService/Review.cs
@@ -1,15 +1,53 @@
 namespace RestaurantService;
 
-public class Review : IReview
+public class Review
 {
-    public void AddReview(string review)
+
+    //constructor
+    public Review()
     {
+        name = "Restaurant Exile";
+        review = "This place rocks! Eat here often!";
+        rating = 5.0;
+        author = "Tester";
+        date = DateTime.Now;
         
     }
 
-    public void PrintReview(string review)
-    {
-        
+
+    //name string
+    public string Name {get; set;}
+
+    private string review;
+    public string Review {get => review; set => review = value;}
+    
+
+    //datetime
+    public DateTime Date {get; set;}
+
+
+    //ratings double
+    private double rating;
+    public double Rating {
+        get => rating; 
+        set {
+            if(value >= 0 && value <= 5.0)
+                rating = value;
+            else
+                throw new ArgumentException("provided value is not valid.");
+            
+            } 
+        }
+
+    
+    //author string
+    private string author {get; set;}
+    public string Author {
+        get => author;
+
+        set => author = value;
     }
+
+
     
 }
