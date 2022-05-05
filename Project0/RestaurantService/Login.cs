39a054b7ea71076521aa428f0a997666b644e5a8
fiopwk <flopwiki@gmail.com>
2022-05-05 10:08:47 -0400

    add login model

---
+++ Project0/RestaurantService/Login.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace RestaurantService
+{
+    public class Login
+    {
+        public Login()
+        {
+            UserName = "";
+            UserPass = "";
+        }
+
+        public string UserName {get; set;}
+        public string UserPass {get; set;}
+
+
+    }
+}
